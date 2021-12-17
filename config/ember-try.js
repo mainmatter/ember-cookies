0d11e7efba546bebad06a047382377166c38ee38
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-12-17 09:21:15 +0100

    add Ember 3.28 scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -70,6 +70,15 @@ module.exports = function () {
           },
         },
         {
+          name: 'ember-lts-3.28',
+          npm: {
+            devDependencies: {
+              'ember-data': '~3.28.0',
+              'ember-source': '~3.28.0',
+            },
+          },
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
