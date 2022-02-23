4138a9294d065c708ccfd725473b7dbae603458b
Sergey Astapov <SergeAstapov@gmail.com>
2022-02-23 14:33:24 -0500

    Run `ember-cli-update` to sync with latest addon blueprint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,19 +20,18 @@ module.exports = {
     // node files
     {
       files: [
-        '.babelrc.js',
-        '.eslintrc.js',
-        '**/.eslintrc.js',
-        '.prettierrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js',
+        './.babelrc.js',
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+        './tests/dummy/.eslintrc.js',
       ],
-      excludedFiles: ['addon/**', 'addon-test-support/**', 'app/**', 'tests/dummy/app/**'],
       parserOptions: {
         sourceType: 'script',
       },
@@ -43,11 +42,9 @@ module.exports = {
       plugins: ['node'],
       extends: ['plugin:node/recommended'],
     },
-    // test files
     {
-      files: ['tests/**/*-test.js'],
-      excludedFiles: ['tests/dummy/**'],
-      plugins: ['qunit'],
+      // test files
+      files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
     },
   ],
--- config/ember-try.js
+++ config/ember-try.js
@@ -1,119 +1,123 @@
-/* eslint-env node */
+'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
 const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
-module.exports = function () {
-  return Promise.all([
-    getChannelURL('release'),
-    getChannelURL('beta'),
-    getChannelURL('canary'),
-  ]).then(urls => {
-    const releaseUrl = urls[0];
-    const betaUrl = urls[1];
-    const canaryUrl = urls[2];
-    return {
-      useYarn: true,
-      scenarios: [
-        {
-          name: 'ember-lts-3.4',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.4.0',
-              'ember-source': '~3.4.0',
-            },
+module.exports = async function () {
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-3.4',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.4.0',
+            'ember-source': '~3.4.0',
           },
         },
-        {
-          name: 'ember-lts-3.8',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.8.0',
-              'ember-source': '~3.8.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.8',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.8.0',
+            'ember-source': '~3.8.0',
           },
         },
-        {
-          name: 'ember-lts-3.12',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.12.0',
-              'ember-source': '~3.12.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.12.0',
+            'ember-source': '~3.12.0',
           },
         },
-        {
-          name: 'ember-lts-3.16',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.16.0',
-              'ember-source': '~3.16.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.16.0',
+            'ember-source': '~3.16.0',
           },
         },
-        {
-          name: 'ember-lts-3.20',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.20.0',
-              'ember-source': '~3.20.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.20.0',
+            'ember-source': '~3.20.0',
           },
         },
-        {
-          name: 'ember-lts-3.24',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.24.0',
-              'ember-source': '~3.24.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.24.0',
+            'ember-source': '~3.24.3',
           },
         },
-        {
-          name: 'ember-lts-3.28',
-          npm: {
-            devDependencies: {
-              'ember-data': '~3.28.0',
-              'ember-source': '~3.28.0',
-            },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.28.0',
+            'ember-source': '~3.28.0',
           },
         },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-data': 'latest',
-              'ember-source': releaseUrl,
-            },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-data': 'latest',
+            'ember-source': await getChannelURL('release'),
           },
         },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-data': 'beta',
-              'ember-source': betaUrl,
-            },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-data': 'beta',
+            'ember-source': await getChannelURL('beta'),
           },
         },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-data': 'canary',
-              'ember-source': canaryUrl,
-            },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-data': 'canary',
+            'ember-source': await getChannelURL('canary'),
           },
         },
-        {
-          name: 'ember-default',
-          npm: {
-            devDependencies: {},
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
+        },
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.28.0',
+            'ember-source': '~3.28.0',
+          },
+          ember: {
+            edition: 'classic',
           },
         },
-        embroiderSafe(),
-        embroiderOptimized(),
-      ],
-    };
-  });
+      },
+      embroiderSafe(),
+      embroiderOptimized(),
+    ],
+  };
 };
