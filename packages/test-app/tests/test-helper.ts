92ef98bb67c236e4afda3a9cf09138775840bca0
bobrimperator <bartholomew.dudzik@gmail.com>
2025-11-08 01:26:09 +0100

    chore: use ember-qunit v9 setup

--- packages/test-app/tests/test-helper.ts
+++ packages/test-app/tests/test-helper.ts
@@ -1,7 +1,10 @@
 import Application from 'test-app/app';
 import config from 'test-app/config/environment';
 import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+setupEmberOnerrorValidation();
+loadTests();
 start();
